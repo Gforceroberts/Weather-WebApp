320128f1fb897f8c58f358af6a4bb5ef941fc495
Gforceroberts <gfroberts@gmail.com>
2014-02-19 21:15:54 +0200

    Added Pressure graph

--- graphs.php
+++ graphs.php
@@ -1,8 +1,13 @@
 <?php include('header.php'); ?>
 
 <?php
-			$SQL = "SELECT HOUR(date) as hour, max(temp) as temp FROM `mydata` WHERE  `date` >= '2014-02-19' group by HOUR(date)";
 
+$today = date("Y-m-d", time() - 60 * 60 * 24);
+
+PRINT $today;
+
+			$SQL = "SELECT HOUR(date) as hour, max(temp) as temp FROM `mydata` WHERE  `date` >= '$today' group by HOUR(date)";
+print $SQL;
           	$maxHourlyTempData = mysql_query($SQL);
           	while ($row = mysql_fetch_array($maxHourlyTempData)) {
 
@@ -56,6 +61,67 @@ $(function () {
 
 		</script>
 
+		
+<?php
+
+$today = date("Y-m-d", time() - 60 * 60 * 24);
+
+PRINT $today;
+
+			$SQL = "SELECT HOUR(date) as hour, max(pressure) as pressure FROM `mydata` WHERE  `date` >= '$today' group by HOUR(date)";
+print $SQL;
+          	$maxHourlyPressureData = mysql_query($SQL);
+          	while ($row = mysql_fetch_array($maxHourlyPressureData)) {
+
+			   $data_pressureCats[] = $row['hour'];
+			   $data_pressureValues[] = $row['pressure'];
+			}
+		?>
+
+	<script type="text/javascript">
+$(function () {
+
+
+
+		var pressureCats = [ <?php echo join($data_pressureCats, ',') ?> ]
+		var pressureData = [ <?php echo join($data_pressureValues, ',') ?> ]
+
+        $('#tempChart').highcharts({
+            title: {
+                text: 'Daily Max Pressure',
+                x: -20 //center
+            },
+            xAxis: {
+                categories: pressureCats
+            },
+            yAxis: {
+                title: {
+                    text: 'Pressure (hPa)'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            tooltip: {
+                valueSuffix: 'hPa'
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                borderWidth: 0
+            },
+            series: [{
+                name: 'Hourly Max',
+                data: pressureData
+            }]
+        });
+    });
+
+
+		</script>		
 
   <?php include('nav.php'); ?>
 
